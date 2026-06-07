72c4ae267be03cbba8d6e24a2b21ec9d1b38c5d6
kashishinsan31 <kashishinsan31@gmail.com>
2026-06-07 20:21:50 +0400

    final

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig(() => {
       tailwindcss(),
       VitePWA({
         registerType: 'autoUpdate',
+        injectRegister: 'auto',
         includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
         manifest: {
           name: 'Waiter Walk',
